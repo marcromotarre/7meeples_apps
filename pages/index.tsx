f764abf260add40a592ef5891ac64906deadfd6e
Marc Romo <marcromotarre@gmail.com>
2021-01-28 00:38:54 +0100

    main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,48 @@
 import React from "react";
 
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import logoRed from "../src/assets/svg/logo-red.svg";
+import siguenosEnInstagram from "../src/assets/svg/siguenos-en-instagram.svg";
 import Link from "next/link";
 
 interface Content {
   title: string;
 }
 
-export default ({ ...props }) => {
-  const { content } = props;
+export default () => {
   return (
-    <div>
-      <h1>{content.title}</h1>
-      <Link href="/notes"> Go tdo notes</Link>
+    <div
+      sx={{
+        display: "grid",
+        gridTemplateColumns: "100%",
+        gridTemplateRows: "50px auto",
+      }}
+    >
+      <Link href="https://www.instagram.com/7meeples.es/">
+        <img
+          sx={{
+            justifySelf: "end",
+            alignSelf: "center",
+            height: "80%",
+            marginRight: "20px",
+          }}
+          src={siguenosEnInstagram}
+          alt="siguenos en instagram"
+        />
+      </Link>
+      <div
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <img
+          sx={{
+            height: "100%",
+            maxWidth: "70%",
+          }}
+          src={logoRed}
+          alt="logo"
+        />
+      </div>
     </div>
   );
 };
-
-export function getStaticProps() {
-  return {
-    props: {
-      content: {
-        title: "This is my really nice app",
-      },
-    },
-  };
-}
